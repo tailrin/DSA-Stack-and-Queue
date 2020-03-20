740977e16ee3db5dfaec45330b3e132547457795
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-03-19 21:48:06 -0500

    added display function

--- stack-work.js
+++ stack-work.js
@@ -7,6 +7,7 @@ starTrek.push('Spock');
 starTrek.push('McCoy');
 starTrek.push('Scotty');
 
+
 const peek = stack => {
     return stack.top.data
 }
@@ -15,7 +16,14 @@ const isEmpty = stack => {
     return stack.top === null
 }
 
-
+const display = stack => { 
+    let current = stack.top;
+    while (current !== null) { 
+        console.log(current.data);
+        current = current.next;
+    }
+ }
+display(starTrek);
 function is_palindrome(s) {
     s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
     // Your code goes here
