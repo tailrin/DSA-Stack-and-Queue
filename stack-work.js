d7b196f7b7320ce95bf25ef6d2cb1c8a1e38dc7d
joshua hahn <tailrin.jh@gmail.com>
2020-03-19 20:22:51 -0600

    added work files for stack and queue classes

--- stack-work.js
+++ stack-work.js
@@ -1,2 +1,24 @@
 const Stack = require('./stack');
 
+
+const starTrek = new Stack();
+starTrek.push('Kirk');
+starTrek.push('Spock');
+starTrek.push('McCoy');
+starTrek.push('Scotty');
+
+const peek = stack => {
+    return stack.top.data
+}
+
+const isEmpty = stack => {
+    return stack.top === null
+}
+
+
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    // Your code goes here
+}
+
+
